d1388e38ddaa0f29f3567b03e0d0c893cc975a62
SAINATH DUTKAR <31874079+SainathDutkar@users.noreply.github.com>
2019-07-29 15:42:10 -0400

    Update DownloadS3.py

--- DownloadS3.py
+++ DownloadS3.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Created on Thu Jul 18 13:47:56 2019
+Created on Mon Jul  8 15:03:30 2019
 
 @author: sainath.dutkar
 """
@@ -10,30 +10,79 @@ import boto3
 from botocore.exceptions import ClientError
 
 
-def download_S3_file(file_name, bucket, object_name):
-    """Upload a file to an S3 bucket
-
-    :param file_name: File to upload
-    :param bucket: Bucket to upload to
-    :param object_name: S3 object name. If not specified then file_name is used
-    :return: True if file was uploaded, else False
-    """
-
-    # If S3 object_name was not specified, use file_name
+"""
+def upload_file(file_name, bucket, object_name,DocSeq,
+                DocType,Venue,Year,DocPrv,PrvReason,DocCode,DocDes,Mime,CourtDiv,ItemType):
     
-
-    # Upload the file
-    s3_client = boto3.client('s3' )
-    try:
-        s3_client.download_file(bucket,object_name,file_name)
+    # If S3 object_name was not specified, use file_name
+    if object_name is None:
+        object_name = file_name
         
+    access_key_id="AKIASZIKFIJSEB74YB6M"
+    secret_access_key= "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"
+        
+    # Upload the file
+    s3_client = boto3.client('s3',aws_access_key_id=access_key_id,
+         aws_secret_access_key= secret_access_key)
+    try:
+     
+        response = s3_client.upload_file(file_name, bucket, object_name,ExtraArgs={'Metadata':
+            {'Docket_Seq_Number': DocSeq,
+             'Docket_Type_Code': DocType,
+             'Docket_Venue_ID':Venue,
+             'Docket_Court_Year':Year,
+             'Document_Privacy_Code':DocPrv,
+             'Document_Privacy_reason':PrvReason,
+             'Document_Code':DocCode,
+             'Document_Description':DocDes,
+             'Document_Mime_Type':Mime,
+             'EFILING_COURT_DIV':CourtDiv,
+             'Item_Type':ItemType
+             }})
+    except ClientError as e:
+        logging.error(e)
+        return False
+    return True
+"""
+
+def upload_file(file_name, bucket, object_name=None):
+    
+    # If S3 object_name was not specified, use file_name
+    if object_name is None:
+        object_name = file_name
+        
+    access_key_id="AKIASZIKFIJSEB74YB6M"
+    secret_access_key= "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"
+        
+    # Upload the file
+    s3_client = boto3.client('s3',aws_access_key_id=access_key_id,
+         aws_secret_access_key= secret_access_key)
+    try:
+     
+        response = s3_client.upload_file(file_name, bucket, object_name,ExtraArgs={'Metadata':
+            {'Docket_Seq_Number': '3005',
+             'Docket_Type_Code': 'CL',
+             'Docket_Venue_ID':'ATL',
+             'Docket_Court_Year':'19',
+             'Document_Privacy_Code':'abc',
+             'Document_Privacy_reason':'abc',
+             'Document_Code':'abc',
+             'Document_Description':'abc',
+             'Document_Mime_Type':'abc',
+             'EFILING_COURT_DIV':'abc',
+             'Item_Type':'abc'
+             }})
     except ClientError as e:
         logging.error(e)
         return False
     return True
 
 
+file = "Doc5.txt"
+#upload_file(file,'njcpoc','Doc5','3005','CL','ATL','19','abcd','abcd','abcd','abcd','abcd','abcd','abcd')
+upload_file(file,'njcpoc','Doc5')
 
-flag = download_S3_file("downloadedDoc.txt","njcpoc","Doc1")
 
-print(flag)
+#flag = upload_file(file,"njcpoc","Doc2")
+
+#print(flag)
